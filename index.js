97f33b5e84855c792174a0cd5a0dc71af26ada4d
DingYiWeiJE <1967374910@qq.com>
2024-05-26 07:26:22 +0800

    正则表达式

--- index.js
+++ index.js
@@ -1,33 +1,4 @@
-function concurRequest(urls, maxNum) {
-  return new Promise((resolve) => {
-    if (urls.length === 0) {
-      resolve([]);
-      return;
-    }
-    const results = []
-    let index = 0
-    let count = 0;
-    
-    async function request() {
-      const i = index
-      const url = urls[i];
-      index++;
-      try {
-        const res = await fetch(url);
-        results[i] = res;
-      } catch (error) {
-        results[i] = error;
-      } finally {
-        count++;
-        if (count === urls.length) {
-          resolve(results)
-        }
-      }
-    }
-
-    const times = Math.min(maxNum, urls.length)
-    for (let i = 0; i < times; i++) {
-      request()
-    }
-  })
-}
+let reg = /[12346！]/g
+let hd = 'y任561天！野'
+console.log(reg.test(hd))
+console.log(hd.match(reg))
